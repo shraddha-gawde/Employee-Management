d7b319e43b8feb5586e61582430998a960900d30
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-03-27 18:55:36 +0530

    full stack

---
+++ frontend/script/dashboard.js
@@ -0,0 +1,215 @@
+const baseURL = "https://employee-management-66vi.onrender.com/";
+
+async function fetchEmployees(page = 1) {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`${baseURL}dashboard?page=${page}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    displayEmployees(data.employees);
+    createPaginationButtons(data.totalPages, page);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function displayEmployees(employees) {
+  const tableBody = document.getElementById("employeeTableBody");
+  tableBody.innerHTML = "";
+  employees.forEach((employee, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+              <td>${index + 1}</td>
+              <td>${employee.firstName}</td>
+              <td>${employee.lastName}</td>
+              <td>${employee.email}</td>
+              <td>${employee.department}</td>
+              <td>${employee.hireDate}</td>
+              <td>${employee.salary}</td>
+              <td><button onclick="editEmployee('${employee._id}')">Edit</button>
+              <button onclick="deleteEmployee('${employee._id}')">Delete</button></td>
+              
+          `;
+    tableBody.appendChild(row);
+  });
+}
+
+async function createPaginationButtons(totalPages, currentPage) {
+  const paginationContainer =
+    document.getElementById("paginationButtons");
+  paginationContainer.innerHTML = "";
+
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.classList.add("paginationButton");
+
+    if (i === currentPage) {
+      button.classList.add("active");
+    }
+
+    button.addEventListener("click", () => {
+      fetchEmployees(i);
+    });
+
+    paginationContainer.appendChild(button);
+  }
+}
+
+fetchEmployees();
+
+
+async function searchEmployeesByName(firstName) {
+    try {
+        const token = localStorage.getItem("token");
+        
+        const response = await fetch(`${baseURL}dashboard/search?search=${firstName}`,{
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        const data = await response.json();
+        displayEmployees(data.employees);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+function searchEmployees() {
+    const firstName = document.getElementById("searchInput").value;
+    if (firstName.trim() !== "") {
+        searchEmployeesByName(firstName);
+    } else {
+        fetchEmployees();
+    }
+}
+async function filterEmployeesByDepartment(department) {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(`${baseURL}dashboard/filter?department=${department}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        displayEmployees(data.employees);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+function filterEmployees() {
+    const department = document.getElementById("departmentFilter").value;
+    if (department !== "") {
+        filterEmployeesByDepartment(department);
+    } else {
+        fetchEmployees();
+    }
+}
+const departmentFilter = document.getElementById("departmentFilter");
+
+departmentFilter.addEventListener("change", () => {
+    filterEmployees();
+});
+
+async function sortEmployeesBySalary(sortOrder = "asc") {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(`${baseURL}dashboard/sort?sortOrder=${sortOrder}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        console.log(data)
+        displayEmployees(data.employees);
+        
+        localStorage.setItem("sortOrder", data.sortOrder)
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+function toggleSortOrder() {
+    const currentSortOrder = localStorage.getItem("sortOrder") || "asc";
+    const newSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
+    localStorage.setItem("sortOrder", newSortOrder);
+    sortEmployeesBySalary(newSortOrder);
+}
+
+toggleSortOrder();
+
+function openAddEmployeeModal() {
+    document.getElementById("addEmployeeModal").style.display = "block";
+}
+
+function closeAddEmployeeModal() {
+    document.getElementById("addEmployeeModal").style.display = "none";
+}
+
+document.getElementById("addEmployeeForm").addEventListener("submit", async function(event) {
+    event.preventDefault(); 
+
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const department = document.getElementById("department").value;
+    const salary = document.getElementById("salary").value;
+
+  
+    const formData = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        department: department,
+        salary: salary
+    };
+
+    try {
+        const response = await fetch(`${baseURL}dashboard/addEmployye`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(formData)
+        });
+        if (response.ok) {
+            console.log("Employee added successfully");
+            closeAddEmployeeModal();
+
+            fetchEmployees();
+        } else {
+            console.error("Error:", response.statusText);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+});
+
+
+async function deleteEmployee(employeeId) {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(`${baseURL}dashboard/delete/${employeeId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data.message);
+            
+            fetchEmployees();
+        } else {
+            console.error("Error:", data.message);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
---
+++ frontend/script/login.js
@@ -0,0 +1,112 @@
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+const loginText = document.querySelector(".titleText .login");
+const signupText = document.querySelector(".titleText .signup");
+const invalidspan = document.getElementById("invalid-text");
+const invalid1span = document.getElementById("invalid1-text");
+
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
+
+const baseURL = "https://employee-management-66vi.onrender.com/";
+
+
+document.getElementById("register_btn").addEventListener("click", registerUser);
+async function registerUser() {
+
+  const email_input = document.getElementById("sign-up-useremail");
+  const password_input = document.getElementById("sign-up-password");
+  const cpassword_input = document.getElementById("sign-up-cpassword");
+
+  const email = email_input.value;
+  const password = password_input.value;
+  const confirmPassword = cpassword_input.value;
+ 
+  try {
+    if (email && password && confirmPassword) {
+      const response = await fetch(`${baseURL}users/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, confirmPassword }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        invalid1span.innerHTML=`${data.msg}`; 
+      invalid1span.style.color = "green"
+      } else {
+        invalid1span.innerHTML=`${data.msg}`; 
+         invalid1span.style.color = "red"
+        
+      }
+    } else {
+      invalid1span.innerHTML= "Enter Your Details"
+      invalid1span.style.color = "red";
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    invalid1span.innerHTML= "Something went wrong"; 
+    invalid1span.style.color = "red"
+  }
+}
+
+
+const loginButton = document.getElementById("log-in-btn");
+
+loginButton.addEventListener("click", loginUser);
+async function loginUser() {
+  const username_input = document.getElementById("log-in-useremail");
+  const password_input = document.getElementById("log-in-password");
+
+  const email = username_input.value;
+  const password = password_input.value;
+  try {
+    if (email && password) {
+      const response = await fetch(`${baseURL}users/login`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        invalidspan.innerHTML = `${data.msg}`;
+        invalidspan.style.color = "green";
+
+        localStorage.setItem("token", data.access_token);
+        console.log(data);
+        setTimeout(() => {}, 1000000);
+        location.href = "../view/dashboard.html";
+      } else {
+        invalidspan.innerHTML = `${data.msg}`;
+        invalidspan.style.color = "red";
+      }
+    } else {
+      invalidspan.innerHTML = "Enter Your Details";
+      invalidspan.style.color = "red";
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    invalidspan.innerHTML = "Something went wrong";
+    invalidspan.style.color = "red";
+  }
+}
